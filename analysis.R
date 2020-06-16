62f30ccdc44c844bcd2ca716f0eb7f36fb6ba398
jonaseichhorst <jonaseichhorst@users.noreply.github.com>
2020-06-16 12:06:13 +0800

    Final Release Version

--- analysis.R
+++ analysis.R
@@ -1,8 +1,8 @@
 library("rethinking")
 
-#input_df <- read.csv("sim_results_k_0_2_mu_2_5.csv")
-
-input_df <- output_df
+input_df <- read.csv("./results/sim_results-0.2-0-9999-2.5-0.001-40-younger-10000-.csv")
+#output_df <- t
+#input_df <- output_df
 input_df$X <- seq(1, length(input_df[,1]))
 
 i_cumsum <- c(0)
@@ -19,24 +19,39 @@ input_df$i_cumsum <- i_cumsum[2:length(i_cumsum)] # drop the first element that 
 input_df$d_cumsum <- d_cumsum[2:length(d_cumsum)] # drop the first element that was 0 and added for coding convenience
 input_df$r_cumsum <- r_cumsum[2:length(r_cumsum)] # drop the first element that was 0 and added for coding convenience
 
-par(mfrow=c(2,2))
 sim_results <- input_df[seq(300, length(input_df$X)+1, by=300),]
-plot(sim_results$d_cumsum, main="Total Deaths By Simulation", xlab="Simulation", ylab="Deaths", col="#1b4c9b")
-dens(sim_results$d_cumsum, main="Density Distribution")
-precis(sim_results$d_cumsum)
-
-plot(sim_results$i_cumsum, main="Total Infected By Simulation", xlab="Simulation", ylab="Infected", col="#1b4c9b")
-dens(sim_results$i_cumsum, main="Density Distribution")
-precis(sim_results$i_cumsum)
-
-HPDI(sim_results$i_cumsum)
-
 sim_results$cfr <- sim_results$d_cumsum / sim_results$i_cumsum
-dens(sim_results$cfr, main="Infection Fatality Rate Distribution", xlab="IFR [%]", ylab="Density")
-precis(sim_results$cfr, prob=0.97, digits=0)
+
 mean(sim_results$cfr)
 sim_results
-input_df[300,]
-input_df[301,]
-input_df[input_df$X == 0, ]
-plot(input_df$X, input_df$i_cumsum)
+plot(input_df$X, input_df$i_cumsum, main="Infections by Simulation", xlab="", xaxt = "n", ylab="Infections", col="#1b4c9b", pch=1)
+dens(sim_results$cfr, main="Implied IFR Distribution", xlab="Implied IFR [%]", ylab="Density")
+
+#x0.15_0.25_10_2.5_older <- sim_results
+#x0.15_0.25_10_2.5_younger <- sim_results
+#x0.15_0.25_10_2.5_US <- sim_results
+#x0.15_0.25_10_2.5_very_old <- sim_results
+
+#b_very_old <- sim_results
+#b_older <- sim_results
+#b_US <- sim_results
+#b_younger <- sim_results
+color_vals <- c("#2176FF", "#A30B37", "#FF9505", "#391463")
+par(mfrow = c(2,1))
+
+dens(b_younger$cfr, main="Baseline Model", xlab="", ylab="Density", xaxt='n', xlim=c(0, 0.15), col=color_vals[1], lty=2, lwd=2)
+dens(b_US$cfr, add = TRUE, col=color_vals[2], lty=1, lwd=2)
+dens(b_older$cfr, add = TRUE, col=color_vals[3], lty=3, lwd=2)
+dens(b_very_old$cfr, add = TRUE, col=color_vals[4], lty=6, lwd=2)
+legend(0.125, 185, c("Younger", "US Baseline", "Older", "Very Old"), bg="white", lty = c(2, 1, 3, 6), col=color_vals, text.col = color_vals)
+
+dens(x0.15_0.25_10_2.5_younger$cfr, main="k: 0.15, social distancing: 0.25, group limit: 10",xlab="Implied IFR [%]", ylab="Density", xlim=c(0, 0.15), col=color_vals[1], lty=2, lwd=2)
+dens(x0.15_0.25_10_2.5_US$cfr, add = TRUE, col=color_vals[2], lty=1, lwd=2)
+dens(x0.15_0.25_10_2.5_older$cfr, add = TRUE, col=color_vals[3], lty=3, lwd=2)
+dens(x0.15_0.25_10_2.5_very_old$cfr, add = TRUE, col=color_vals[4], lty=6, lwd=2)
+legend(0.125, 48, c("Younger", "US Baseline", "Older", "Very Old"), bg="white", lty = c(2, 1, 3, 6), col=color_vals, text.col = color_vals)
+
+
+#legend(1, 95, legend=c("Line 1", "Line 2"),
+#       col=c("red", "blue"), lty=1:2, cex=0.8,
+#       title="Line types", text.font=4, bg='lightblue')
--- main.R
+++ main.R
@@ -1,66 +1,57 @@
-#0: set up simulation with p=0 and a randomly picked number of sick patients
-### Aferwards, for each period:
-#1: determine the number p_inf of potenially infectuous patients (have been infected within 14 days)
-#2: determine the total sum s_inf of secondary infections across all infectuous primary infections
-#3: Randomly set s_inf people from susceptible population to become infected in p+1 adjusted for the susceptible
-#   portion of the total active (S + R) portion of the population
-#4: For new patients, determine their mortality risk and set their period of death or recovery
-
-# Target Output:
-# a data frame with n=population size rows and 4 columns:
-# 1) is person still susceptible
-# 2) what period (if any) did that person get infected
-# 3) what period (if any) did that person die
-# 4) what period (if any) did that person recover
-
-
 library("rethinking")
 library("parallel")
 library("collections")
 
 set.seed(101)
 
+###############################
 # return the ifr by age bracket
 calculate_ifr <- function(){
-  # TODO: Further define the correct mortality by age bracket
-  #likelihood <- c(0.01, 0.01, 0.0001, 0.04, 0.20, 0.86, 3.50, 10.3, 27.06, 40.98, 17.06)
-  mortality_likelihood <- c(0.01, 0.01, 0.0001, 0.04, 0.20, 0.86, 3.50, 10.3, 27.06, 40.98, 60.06) # last value cleansed
+  mortality_likelihood <- c(0.01, 0.0001, 0.04, 0.20, 0.86, 3.50, 10.3, 27.06, 40.98, 17.06)
   return(mortality_likelihood / sum(mortality_likelihood))
 }
 
-
-create_population_network <- function(population = 10000){
-  start <- proc.time()
-  contacts <- rnbinom(population, 2, 0.05)+1 #everybody must have at least one contact
+##############################
+# population is connected by:
+# 1) randomly determining the number of links each person will have
+# 2) assigning those links per individual with clustering of similarly aged participants
+setup_population_network <- function( ages ){
   
-  population_idx <- seq(population)
+  if(rebuild_pop_network == FALSE){
+    p_pairs <- read.csv("./params/pop_network_10000.csv")
+    return(p_pairs)
+  }
+  
+  pop_count <- length(ages)
+  contacts <- rnbinom(pop_count, 2, 0.05)+1 #everybody must have at least one contact
+  
+  population_idx <- seq(pop_count)
   p_pairs <- c()
   
-  for (i in 1:population){
-    new_links <- sample(population_idx, contacts[i], prob=contacts, replace=TRUE)
+  for (i in 1:pop_count){
+    # we cluster by calculating squared (age) distance of people to provide heavier weightage for similarly aged
+    prob <- abs(ages-ages[i])^2
+    prob <- max(prob) - prob
+    
+    new_links <- sample(population_idx, contacts[i], prob=prob, replace=TRUE)
     new_pairs <- cbind(rep(i, length(new_links)), new_links)
     p_pairs <- rbind(p_pairs, new_pairs)
   }
-  end <- proc.time()
-  print(end - start) 
   return(p_pairs)
 }
 
-#x <- create_population_network(5000)
-
-
-# initialitze data frame
-# set up age distribution of population 
-setup_population <- function( N_pop_size = 10000 ){
+###################################
+setup_population <- function( N_pop_size = 10000 , age_range = "US"){
   s <- rep(1,N_pop_size) # All are susceptible
   i <- rep(-1,N_pop_size) # nobody infected & contagious
   d <- rep(-1,N_pop_size) # nobody died
   r <- rep(-1,N_pop_size) # nobody recovered
-  a <- setup_population_ages( N_pop_size )
+  a <- setup_population_ages( N_pop_size, age_range )
   return(data.frame(s, i, d, r, a))
 }
 
-setup_population_ages <- function( N_pop_size = 10000 ){
+###################################
+setup_population_ages <- function( N_pop_size = 10000, age_range = "US" ){
   # age distribution:
   # work with US demographic data in 5 year increments, then simply randomize a bit to have nicer distribution
   US_age_range <- c(19.81, 20.2, 20.88, 21.09, 21.87, 23.56, 22.13, 21.56, 19.72, 20.74, 20.89, 21.94, 20.33, 17.08, 13.4, 9.26, 6.13, 6.55)
@@ -79,20 +70,48 @@ setup_population_ages <- function( N_pop_size = 10000 ){
   very_old_age_range[(length(US_age_range)/2):length(US_age_range)] <- US_age_range[(length(US_age_range)/2):length(US_age_range)]*2
   very_old_age_range[(length(US_age_range)/5*4):length(US_age_range)] <- US_age_range[(length(US_age_range)/5*4):length(US_age_range)]*3
   
-  a <- sample(0:17, size = N_pop_size, replace = TRUE, prob=very_old_age_range)
+  if(age_range == "US")
+    prob <- US_age_range  
+  if(age_range == "younger")
+    prob <- younger_age_range
+  if(age_range == "older")
+    prob <- older_age_range
+  if(age_range == "very_old")
+    prob <- very_old_age_range
+  
+  a <- sample(0:17, size = N_pop_size, replace = TRUE, prob=prob)  
   a <- a*5+sample(0:4, N_pop_size, replace = TRUE)
   return(a)  
 }
 
+
+#######################################
 get_infectious_p <- function( period, pop ){
   # check for any patient that was infected 3-8 days ago
-  i <- which(pop$i %in% pop$i[(pop$i >= 0 & pop$i > (period - 8) & pop$i < (period - 3))])
-  return(i)
+  return(which(pop$i %in% pop$i[(pop$i >= 0 & pop$i > (period - 8) & pop$i < (period - 3))]))
 }
 
-calc_secondary_infect <- function( period , pop , 
-                                     N_initial_p_perc_pop = 0.001, seeding = FALSE, 
-                                     agg_distancing = c(0.05, 10), mod_distancing = c(0.02, 50)){
+#######################################
+get_susceptible_p <- function( period, pop ){
+  # any susceptible patient
+  return(which(pop$s %in% pop$s[pop$s == 1]))
+}
+
+#######################################
+get_active_p <- function( period, pop ){
+  # any patient who's been infected but hasn't recovered or died yet
+  return(which(pop$i %in% pop$i[(pop$i >= 0 & (period < pop$r) | (period < pop$d))]))
+}
+
+#######################################
+get_last_period_transmissions <- function( period, pop){
+  return(which(pop$i %in% pop$i[pop$i == (period - 1) & period > 0]))
+}
+
+
+#######################################
+calc_secondary_infect <- function( mu=2.5, k=0.2, period , social_distancing_status, pop , 
+                                     N_initial_p_perc_pop = 0.001, seeding = FALSE){
   
   # patients between 4-7 days post infection are most dangerous
   n_infectious_patients <- length(get_infectious_p(period , pop))
@@ -104,39 +123,57 @@ calc_secondary_infect <- function( period , pop ,
   if(seeding){ # seed the simulation first with a constant stream of inbound patients  
     
     new_patients_count <- as.numeric(round(N_initial_p_perc_pop * nrow(pop), digits=0))
+    
+    # ensure we always have at least one new infection
+    if (new_patients_count == 0)
+      new_patients_count <- 1
+    
     secondary_infections <- rep(1, new_patients_count)
   }
   else{
-    # TODO: normalize for difference in infectiousness by day:
+    
     # currently flat infectiousness during peak days assumed (day 4-7)
-    # Mu & K default (2.5 & 0.1) set in accordance with Endo et al: https://wellcomeopenresearch.org/articles/5-67
-    secondary_infections <- rnbinom(n_infectious_patients / 4, 0.2, mu=2.5)
+    # Endo et al estimate Mu & K as 2.5 and 0.1 but the default value for k is set
+    # slightly more conservative at 0.2
+    # https://wellcomeopenresearch.org/articles/5-67
+    secondary_infections <- rnbinom(n_infectious_patients / 4, k, mu=mu)
     
+    # apply social distancing factor to reduce number of infections
     
-    # apply distancing measures at thresholds to reduce super spreader events
-    if (active_cases / length(pop$s[pop$s>0]) > mod_distancing[1]){
-      secondary_infections[secondary_infections > mod_distancing[2]] <- 0
-    }
-    if (active_cases / length(pop$s[pop$s>0]) > agg_distancing[1]){
-      secondary_infections[secondary_infections > agg_distancing[2]] <- 0
+    secondary_infections <- secondary_infections * rbinom(length(secondary_infections), 1, (1 - social_distancing_status[period, 1]))
+
+    if (social_distancing_status[period, 2] > 0)
+    {
+        secondary_infections[secondary_infections > social_distancing_status[period, 2]] <- 0
     }
   }
   return(secondary_infections)
 }
 
+################################
+manage_social_distancing <- function(new_patients_count, pop, period, social_distancing_status, 
+                                     sd_ap_threshold = 0.01, sd_duration = 21, sd_reduction = 0.3, sd_max_group_size = 15){
+  ap_count <- length(get_active_p(period, pop))
+  if (social_distancing_status[period] == 0 & ap_count > sd_ap_threshold * nrow(pop)){
+    end_period <- min((period + sd_duration), N_periods)
+    social_distancing_status[period:end_period,1] <- sd_reduction
+    social_distancing_status[period:end_period,2] <- sd_max_group_size
+  }
+  return(social_distancing_status)
+}
+
+#################################
 identify_new_patients <- function(sec_infections, pop, period, p_pairs, directed = TRUE){
-  # randomly pick patients from the susceptible pool - by using prob = s we can ensure we only pick
-  # susceptible patients as the others have s:0, i.e. will have a 0 probability
   
   if (directed){
     if(sum(sec_infections) > sum(pop$s)){
       print("exceeded")
       return(c())
     }
+    
     # 1. if a patient causes secondary infections
     # 2.  find all connected people
     # 3.  infect the maximum possible number of secondary infections up to the expected maximum secondary infections
-
     infectious_p <- get_infectious_p(period, pop)
     new_patients_idx <- c()
 
@@ -151,11 +188,13 @@ identify_new_patients <- function(sec_infections, pop, period, p_pairs, directed
           sec_infections[i] = length(potential_p)
         }
         
+        # randomly pick the number of patients to infect from the connected patients
         new_patients_idx <- c(new_patients_idx, sample(potential_p, sec_infections[i]))
       }
     }
   }
-  else{ # randomly pick new patients to infect
+  # randomly pick new patients to infect
+  else{
     new_patients_idx <- sample(1:length(pop[,1]), sum(sec_infections), prob=pop$s)
   }
     
@@ -163,11 +202,16 @@ identify_new_patients <- function(sec_infections, pop, period, p_pairs, directed
   return(new_patients_idx)
 }
 
+################################
+sim_city_epidemic <- function( p_size = 10000, age_range = "US", 
+                               N_periods = 300, N_seed = 5, N_initial_p_perc_pop = 0.001,
+                               mu = 2.5, k = 0.2,
+                               sd_ap_threshold = 0.01, sd_duration = 21, sd_reduction = 0.3, sd_max_group_size = 15) {
 
-sim_city_epidemic <- function( p_size = 10000, p_pairs, N_periods = 300, N_seed = 5) {
-  
   ifr <- calculate_ifr( )
-  pop <- setup_population( p_size )
+  pop <- setup_population( p_size , age_range)
+  p_pairs <- setup_population_network(pop$a)
+  social_distancing_status <- matrix(rep(0,600), ncol = 2)
   
   for ( period in 1:N_periods ) {
   
@@ -179,11 +223,16 @@ sim_city_epidemic <- function( p_size = 10000, p_pairs, N_periods = 300, N_seed 
     
     seed <- ifelse(period <= N_seed, TRUE, FALSE)
 
-    secondary_infections <- calc_secondary_infect(period = period, pop = pop, seeding = seed)
-    new_patients_count <- sum(secondary_infections)
+    secondary_infections_idx <- calc_secondary_infect(mu = mu, k = k, period = period, N_initial_p_perc_pop = N_initial_p_perc_pop,
+                                                      social_distancing_status = social_distancing_status, 
+                                                      pop = pop, seeding = seed)
+    
+    new_patients_count <- sum(secondary_infections_idx)
+    social_distancing_status <- manage_social_distancing(new_patients_count, pop, period, social_distancing_status, 
+                                                         sd_ap_threshold, sd_duration, sd_reduction, sd_max_group_size)
     
     if (new_patients_count > 0){
-      new_patients_idx <- identify_new_patients(sec_infections = secondary_infections, pop = pop, p_pairs = p_pairs, period = period, directed=!seed)
+      new_patients_idx <- identify_new_patients(sec_infections = secondary_infections_idx, pop = pop, p_pairs = p_pairs, period = period, directed=!seed)
       
       # set the susceptible patients now as infected and record their infection period
       pop$s[new_patients_idx] <- 0
@@ -198,14 +247,14 @@ sim_city_epidemic <- function( p_size = 10000, p_pairs, N_periods = 300, N_seed 
       # set the dates for the recovery or decease for the people whose fate we now know
       recovery <- rep(1, length(new_patients_idx)) - mortality
       
-      pop$d[new_patients_idx] <- mortality * (period + 21) # patients die after 21 days
-      pop$r[new_patients_idx] <- recovery * (period + 21) # patients recover within 14 days
+      pop$d[new_patients_idx] <- mortality * (period + 18) # patients die after 18 days
+      pop$r[new_patients_idx] <- recovery * (period + 22) # patients recover within 22 days
     }
   }
   
   # Dataframe summarizing
   {
-  # summarize the events by day and return 
+  # summarize the events by day and return
   new_i_period <- new_d_period <- new_r_period <- total_s_period <- 0
   for (p in 1:N_periods){
     new_i_period <- c(new_i_period, length(pop$i[pop$i==p]))
@@ -214,29 +263,23 @@ sim_city_epidemic <- function( p_size = 10000, p_pairs, N_periods = 300, N_seed 
     total_s_period <- c(total_s_period, length(pop$s) - length(pop$i[(pop$i > -1) & (pop$i < p)]))
   }
   
-  sim_result_by_day <- data.frame(seq(1, length(new_i_period) - 1), new_i_period[-1], new_d_period[-1], new_r_period[-1], total_s_period[-1])
-  names(sim_result_by_day)[1] <- "X"
+  sim_result_by_day <- data.frame(seq(1, length(new_i_period) - 1), new_i_period[-1], new_d_period[-1], new_r_period[-1], total_s_period[-1], social_distancing_status[,1], social_distancing_status[,2])
+  colnames(sim_result_by_day) <- c("X", "new_i_period", "new_d_period", "new_r_period", "total_s_period", "sd_reduction", "sd_max_group_size")
   }
 
   return(sim_result_by_day)
-  
 }
 
-#p_pairs <- read.csv("10000_population_p_pairs.csv")
-#p_pairs$X <- NULL
-
-#p_pairs <- create_population_network(1000)
-#t <- sim_city_epidemic()
-
-
-# set up simulation
-N_pop_size <- 10000
-
-run_simulation <- function(iterations = 1){
-  p_pairs <- create_population_network(N_pop_size)
-  results_df <- setNames(data.frame(matrix(ncol = 4, nrow = 0)), c("new_i_period", "new_d_period", "new_r_period", "total_s_period"))  
+run_simulation <- function(iterations = 1, row){
+  results_df <- data.frame(matrix(ncol = 6, nrow = 0))
   for (i in 1:iterations){
-    x <- sim_city_epidemic(p_size = N_pop_size, p_pairs = p_pairs)
+    x <- sim_city_epidemic(p_size = sim_params[row,]$pop, age_range = sim_params[row,]$age,
+                           k = sim_params[row,]$k_vals, 
+                           mu = sim_params[row,]$mu_vals,
+                           N_initial_p_perc_pop = sim_params[row,]$seed_inf_p,
+                           sd_reduction = sim_params[row,]$sd,
+                           sd_max_group_size = sim_params[row,]$gs
+                           )
     results_df <- rbind(results_df, x)
   }
   return(results_df)
@@ -244,31 +287,40 @@ run_simulation <- function(iterations = 1){
 
 # create cluster object
 numCores <- detectCores()
-target_trials <- 0
-trials_per_core <- target_trials / numCores
 
 cl <- makeCluster(numCores)
 clusterExport(cl, c("sim_city_epidemic", "run_simulation", 
                     "calculate_ifr", "setup_population",
-                    "calc_secondary_infect", "get_infectious_p",
-                    "identify_new_patients", "create_population_network", "setup_population_ages"))
+                    "calc_secondary_infect", "get_infectious_p", "get_active_p", "manage_social_distancing", 
+                    "identify_new_patients", "setup_population_network", "setup_population_ages"))
 
-clusterExport(cl, c("trials_per_core", "N_pop_size"))
+sim_params <- read.csv("./params/sim_params.csv")
 
-start <- proc.time()
-cluster_results <- clusterEvalQ(cl, run_simulation(trials_per_core))
+for (r in 1:nrow(sim_params)){
+  start <- proc.time()
+  
+  rebuild_pop_network <- sim_params[r,]$rebuild_pop_network
+  trials_per_core <- sim_params[r,]$sims / numCores
+  clusterExport(cl, c("trials_per_core", "N_pop_size", "sim_params", "rebuild_pop_network", "r", "N_periods"), envir=environment())
+  cluster_results <- clusterEvalQ(cl, run_simulation(trials_per_core, r))
+  
+  # merge the different cluster results back into one frame
+  output_df <- cluster_results[[1]]
+  for (i in 2:numCores){
+    output_df <- rbind(output_df, cluster_results[[i]])
+  }
+  
+  rownames(output_df) <- seq(length=nrow(output_df))
 
-# merge the different cluster results back into one frame
-output_df <- cluster_results[[1]]
-for (i in 2:numCores){
-  output_df <- rbind(output_df, cluster_results[[i]])
+  file_name <- paste("./results/sim_results", 
+                     sim_params[r,1], sim_params[r,2], sim_params[r,3], sim_params[r,4], 
+                     sim_params[r,5], sim_params[r,6], sim_params[r,7], sim_params[r,8],  
+                     ".csv", sep = "-")
+  write.csv(output_df, file_name)
+  end <- proc.time()
+  print(end - start)
+  
 }
-
-rownames(output_df) <- seq(length=nrow(output_df))
-end <- proc.time()
-print(end - start) 
 
 # close
 stopCluster(cl)
-
-#write.csv(output_df,"sim_results_k_0_2_mu_2_5_with_dist_02_50_05_10_very_old.csv")
